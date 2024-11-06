1871c79e5deef5745cbe9733f9876e75135e4544
Cyr <cyril.kocab@orange.fr>
2024-11-06 20:59:12 +0100

    Updated pipeline.py
    IMPORTANT : model, encoder and imputer are sensible to feature names order.
    The order that should be respected is quantitative features ordered from A to Z, then qualitative features ordered from A to Z.

--- pipeline.py
+++ pipeline.py
@@ -9,12 +9,21 @@ def Pipeline(new_data):
     file.close()
 
     quali_vars = ["Etiquette_GES", "Logement", "Type_bâtiment"]
-    quali_OHE = encoder.transform(new_data[quali_vars]).toaray()
+    quali_OHE = encoder.transform(new_data[quali_vars]).toarray()
     quali_OHE_df = pd.DataFrame(quali_OHE, index=new_data.index, columns=encoder.get_feature_names_out(quali_vars))
+    quali_OHE_df = quali_OHE_df.drop(["Etiquette_GES_C","Logement_ancien","Type_bâtiment_appartement"], axis=1, )
 
-    quanti = new_data[new_data.columns.difference(quali_vars)]
+    with open(os.path.join("objects","median_imputer.pkl"), "rb") as file:
+        imputer = pk.load(file)
+    file.close()
+    
+    quanti_vars = ['Année_construction','Code_postal_(brut)','Coût_total_5_usages',
+                   'Hauteur_sous-plafond','Nombre_niveau_logement','Surface_habitable_logement']
+    quanti = new_data[quanti_vars]
+    quanti_imputed = imputer.transform(quanti)
+    quanti_imputed_df = pd.DataFrame(quanti_imputed, index=new_data.index, columns=imputer.get_feature_names_out(quanti_vars))
 
-    new_data = pd.concat([quanti, quali_OHE_df], axis=1)
+    new_data = pd.concat([quanti_imputed_df, quali_OHE_df], axis=1)
 
     with open(os.path.join("models","rf_tuned_classification.pkl"), "rb") as file:
             model = pk.load(file)
